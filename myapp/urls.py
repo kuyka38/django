c14999beafb0bbe3b268bb4bc46cf4100dd98849
kuyka38 <kunjikuyka@gmail.com>
2025-07-15 19:40:52 +0300

    Initial commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,13 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views. home),
-    path('about/', views.about),
-    path('contact/', views.contact),
-    path('gallery/', views.gallery),
-    path('products/', views.products),
+    path('', views. home,name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('products/', views.products, name='products'),
+
+    path('login/', views.login_view, name='login'),
+   path('', views.register, name='register'),
+
 ]
